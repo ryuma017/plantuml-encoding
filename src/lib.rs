24c82a58e8298b4be66964eb96a3a381ab910d20
ryuma017 <tgcrym01@gmail.com>
2022-07-08 10:45:03 +0900

    add `it_decodes_plantuml_hex` test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-pub fn encodes_plantuml_hex(plantuml: &str) -> String {
+pub fn encode_plantuml_hex(plantuml: &str) -> String {
     let hex = hex::encode(plantuml);
 
     String::from("~h") + &hex
@@ -11,8 +11,16 @@ mod tests {
     #[test]
     fn it_encodes_plantuml_hex() {
         assert_eq!(
-        encodes_plantuml_hex("@startuml\nPUML -> RUST: HELLO \n@enduml"),
+        encode_plantuml_hex("@startuml\nPUML -> RUST: HELLO \n@enduml"),
         "~h407374617274756d6c0a50554d4c202d3e20525553543a2048454c4c4f200a40656e64756d6c"
+        );
+    }
+
+    #[test]
+    fn it_decodes_plantuml_hex() {
+        assert_eq!(
+            decode_plantuml_hex("~h407374617274756d6c0a50554d4c202d3e20525553543a2048454c4c4f200a40656e64756d6c"),
+            "@startuml\nPUML -> RUST: HELLO \n@enduml"
         );
     }
 }
