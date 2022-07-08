279fc5b8679cd5e5c705d448aef563ae9a18e5d7
ryuma017 <tgcrym01@gmail.com>
2022-07-08 10:59:19 +0900

    add type conversion and convert functions to generic functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-pub fn encode_plantuml_hex(plantuml: &str) -> String {
-    let hex = hex::encode(plantuml);
+pub fn encode_plantuml_hex<T: AsRef<str>>(plantuml: T) -> String {
+    let hex = hex::encode(plantuml.as_ref());
 
     String::from("~h") + &hex
 }
 
-pub fn decode_plantuml_hex(hex: &str) -> String {
-    let plantuml_hex_trimmed = hex.trim_start_matches("~h");
+pub fn decode_plantuml_hex<T: AsRef<str>>(hex: T) -> String {
+    let plantuml_hex_trimmed = hex.as_ref().trim_start_matches("~h");
 
     let decoded_bytes = hex::decode(plantuml_hex_trimmed).unwrap();
 
@@ -30,5 +30,14 @@ mod tests {
             decode_plantuml_hex("~h407374617274756d6c0a50554d4c202d3e20525553543a2048454c4c4f200a40656e64756d6c"),
             "@startuml\nPUML -> RUST: HELLO \n@enduml"
         );
+    }
+
+    #[test]
+    #[allow(clippy::unnecessary_to_owned)]
+    fn it_encodes_plantuml_hex_from_string() {
+        assert_eq!(
+            encode_plantuml_hex("@startuml\nPUML -> RUST: HELLO \n@enduml".to_string()),
+            "~h407374617274756d6c0a50554d4c202d3e20525553543a2048454c4c4f200a40656e64756d6c"
+        )
     }
 }
