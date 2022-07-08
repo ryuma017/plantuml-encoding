64cf2aee0d08f1b85f26348e632805a5500a125d
ryuma017 <tgcrym01@gmail.com>
2022-07-08 10:14:40 +0900

    add a first test

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn it_encodes_plantuml_hex() {
+        assert_eq!(
+        encodes_plantuml_hex("&startuml\nPUML -> RUST: HELLO \n@enduml"),
+        ""
+        );
     }
 }
